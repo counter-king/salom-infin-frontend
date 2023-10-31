a4ec2b4f5e5b77f4067d797f1fb7ef4184529fd1
Muhammad <imuhammadibragimov1994@gmail.com>
2023-10-31 12:00:15 +0500

    feat(deps): обновление пакетов

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
+import Components from 'unplugin-vue-components/vite'
+import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     svgLoader(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+      dirs: ['src/components/UI'],
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+    })
   ],
   resolve: {
     alias: {
