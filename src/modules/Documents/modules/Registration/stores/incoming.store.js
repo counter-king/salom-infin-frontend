4ef64533a1cccbac7039b9be9908160f6de0a9cd
Muhammad <imuhammadibragimov1994@gmail.com>
2023-11-24 15:05:26 +0500

    feat: incoming formani o'zgartirish funksiyasi qo'shildi

--- src/modules/Documents/modules/Registration/stores/incoming.store.js
+++ src/modules/Documents/modules/Registration/stores/incoming.store.js
@@ -201,14 +201,37 @@ export const useRegIncoming = defineStore("reg-incoming", {
 
       this.detailModel.__copy_prototype = combineKeys(this.headers, data)
       setValuesToKeys(this.detailModel, data)
-      this.detailModel.__reviewers = this.detailModel.reviewers
+      this.detailModel.__reviewers = data.reviewers.map(item => {
+        return {
+          ...item,
+          __userId: item.user.id
+        }
+      })
     },
     /*
     * Изменить документ
     * */
     async actionUpdateDocument() {
+      let model = {
+        ...this.detailModel,
+        reviewers: this.detailModel.__reviewers.map(item => {
+          if(item.__userId) {
+            return {
+              id: item.id,
+              user: item.__userId,
+              document: item.document
+            }
+          }
+          else {
+            return {
+              user: item.id,
+            }
+          }
+        })
+      }
+
       try {
-        await fetchUpdateDocument({ id: this.detailModel.id, body: this.detailModel })
+        await fetchUpdateDocument({ id: this.detailModel.id, body: model })
         await this.actionGetById({ id: this.detailModel.id })
         dispatchNotify('Документ создан', 'Документ изменен', COLOR_TYPES.SUCCESS)
       }
