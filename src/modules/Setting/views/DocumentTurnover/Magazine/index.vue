975fe3f2e471f1334e621254ba00a316b23202b7
abdulahadxon_a <abdulahadxon_a@rtrd12.asakabank.com>
2023-11-30 17:47:29 +0500

    Журнал chala

--- src/modules/Setting/views/DocumentTurnover/Magazine/index.vue
+++ src/modules/Setting/views/DocumentTurnover/Magazine/index.vue
@@ -1,11 +1,76 @@
 <script setup>
-
+// Core
+import { useI18n } from 'vue-i18n'
+// Store
+import { useSearchStore } from "./stores/index";
+// Components
+import { CardTable} from "@/components/Table/index";
+// Composable
+const { t } = useI18n()
+const searchStore = useSearchStore();
 </script>
 <template>
-  <div>
-    Salom Magazine
+  <div class="flex flex-col h-full">
+    <div class="flex mb-5 justify-between items-center">
+      <h1 class="text-2xl font-bold text-primary-900">Журнал</h1>
+
+      <base-button
+        label="create"
+        icon-left="AddIcon"
+        rounded
+        type="button"
+        @click="sidebar = !sidebar"
+      />
+    </div>
+
+    <card-table
+      class="m-e-table"
+      :value="searchStore.list"
+      :headers="searchStore.headers"
+    >
+      <template #number="{ data }">
+        {{data.number}}
+      </template>
+
+      <template #nameTechnique="{ data }">
+        {{data.nameTechnique}}
+      </template>
+
+      <template #id="{ data }">
+        {{data.id}}
+      </template>
+
+      <template #inb="{ data }">
+        {{data.inb}}
+      </template>
+
+      <template #action>
+        <div class="flex justify-end gap-2">
+          <base-button
+            size="small"
+            icon-left="DownloadMinimalistic"
+            only-icon
+            rounded
+            text
+            icon-width="18"
+            icon-height="18"
+            class="group bg-white text-greyscale-500 hover:text-primary-500"
+          />
+
+          <base-button
+            size="small"
+            icon-left="EyeIcon"
+            only-icon
+            rounded
+            text
+            icon-width="18"
+            icon-height="18"
+            class="group bg-white text-greyscale-500 hover:text-warning-500"
+          />
+        </div>
+      </template>
+    </card-table>
   </div>
 </template>
-<style>
 
-</style>
+
