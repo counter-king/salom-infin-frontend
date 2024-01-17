919132285b919e5a3cf754577f1ae69ec6601195
Azamjon Kirgizboev <azamjon_k@asakabank.uz>
2024-01-17 17:38:01 +0500

    Editor upgraded

--- src/components/UI/BaseFroalaEditor.vue
+++ src/components/UI/BaseFroalaEditor.vue
@@ -84,9 +84,15 @@ const config = {
 	background: var(--greyscale-90)!important;
 }
 
-.fr-second-toolbar,
-.fr-wrapper a {
-	display: none!important;
+//.fr-second-toolbar,
+//.fr-wrapper a {
+//	display: none!important;
+//}
+
+a[href='https://www.froala.com/wysiwyg-editor?k=u'] {
+	font-size: 0px !important;
+	padding: 0px !important;
+	height: 0px !important;
 }
 
 p[data-f-id="pbf"] {
