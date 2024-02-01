846c3cfcb2ffef9951850831a54b66cc256ff113
Muhammad <imuhammadibragimov1994@gmail.com>
2024-02-01 17:31:05 +0500

    добавлены счетчики

--- src/modules/Documents/stores/count.store.js
+++ src/modules/Documents/stores/count.store.js
@@ -1,7 +1,6 @@
 // Core
 import { defineStore } from 'pinia'
 // Stores
-import { useThemeStore } from '@/stores/theme.store'
 import { useBoxesCommonStore } from '../modules/Boxes/stores/common.store'
 // Services
 import { fetchDocumentCountList } from '../services/count.service'
@@ -9,21 +8,13 @@ export const useDocumentCountStore = defineStore("document-count", {
   state: () => ({}),
   actions: {
     async actionDocumentCountList() {
-      const themeStore = useThemeStore()
       const boxesStore = useBoxesCommonStore()
       let { data } = await fetchDocumentCountList()
 
-      Object.entries(data.boxes).map(([key, value]) => {
+      Object.keys(data.boxes).map(key => {
         let route = boxesStore.routes.children.find(route => route.name === key)
 
-        if(route) {
-          route.count = data.boxes[key]
-          boxesStore.routes.count = boxesStore.routes.count + value
-        }
-        else {
-          let document = themeStore.header?.find(header => header.name === 'document')
-          document.count = value
-        }
+        route ? route.count = data.boxes[key] : boxesStore.routes.count = data.boxes.all
       })
     }
   }
