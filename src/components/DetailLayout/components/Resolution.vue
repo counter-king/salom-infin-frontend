31b921411756bf0c9c3e65c8712f6d0844200b81
imuhammadibragimov1994@gmail.com <muhammad1994muhammad>
2024-07-28 16:04:37 +0500

    feat: изменение детальный страницы ящики

---
+++ src/components/DetailLayout/components/Resolution.vue
@@ -0,0 +1,45 @@
+<script setup>
+// Components
+import {
+  ResolutionChairmanTemplate,
+  ResolutionDeputyChairmanTemplate,
+  ResolutionDefaultTemplate
+} from '@/components/Templates'
+import { TreeUsers } from '@/components/Tree'
+// Enums
+import { ROLES } from '@/enums'
+
+// Macros
+const props = defineProps({
+  resolution: {
+    type: Object,
+    default: () => {}
+  },
+  treeItems: {
+    type: Object,
+    default: () => {}
+  }
+})
+</script>
+
+<template>
+  <div class="resolution-view">
+    <div class="h-[calc(100vh-303px)] overflow-y-auto p-6">
+      <div class="shadow-block min-h-[400px] border-[0.094rem] border-greyscale-200 rounded-lg p-6">
+        <template v-if="ROLES.CHAIRMAN === props.resolution.reviewer?.role?.name">
+          <resolution-chairman-template :resolution="props.resolution" />
+        </template>
+
+        <template v-else-if="ROLES.FIRST_DEPUTY_CHAIRMAN || ROLES.DEPUTY_CHAIRMAN === props.resolution.reviewer?.role?.name">
+          <resolution-deputy-chairman-template :resolution="props.resolution" />
+        </template>
+
+        <template v-else>
+          <resolution-default-template :resolution="props.resolution" />
+        </template>
+      </div>
+
+      <tree-users :tree-items="props.treeItems" />
+    </div>
+  </div>
+</template>
---
+++ src/components/FieldGroupCollapse.vue
@@ -0,0 +1,48 @@
+<script setup>
+// Core
+import { ref, computed } from 'vue'
+// Components
+import FieldGroups from './FieldGroups.vue'
+// Utils
+import { combineKeys } from '@/utils'
+// Macros
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => {}
+  },
+  headers: {
+    type: Array,
+    default: () => []
+  },
+  max: {
+    type: Number,
+    default: 5
+  }
+})
+// Reactive
+const count = ref(1)
+// Computed
+const value = computed(() => combineKeys(props.headers, props.data))
+const visible = computed(() => count.value & 1 ? value.value.slice(0, props.max) : value.value)
+</script>
+
+<template>
+  <div class="field-group-collapse-view bg-greyscale-50 relative border border-greyscale-90 rounded-xl py-4 px-5">
+    <slot>
+      <field-groups :headers="props.headers" :data="visible">
+      </field-groups>
+    </slot>
+
+    <button
+      type="button"
+      class="flex items-center justify-center w-8 h-8 bg-greyscale-70 rounded-lg border border-greyscale-200 absolute top-5 right-5 p-1"
+      @click="count++"
+    >
+      <base-iconify
+        :icon="`solar:alt-arrow-${count & 1 ? 'down' : 'up'}-outline`"
+        class="!w-5 !h-5"
+      />
+    </button>
+  </div>
+</template>
