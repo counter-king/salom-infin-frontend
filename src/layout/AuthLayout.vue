104cd71678b006880dd95fa95d359c9f5615ef50
Odinayev Muhammad Komil og'li <imuhammadibragimov1994@gmail.com>
2024-07-31 14:01:54 +0500

    feat: редизайн таблицы

--- src/layout/AuthLayout.vue
+++ src/layout/AuthLayout.vue
@@ -7,10 +7,10 @@ import LanguageDropdown from '@/components/Toolbar/LanguageDropdown.vue'
   <div class="auth-layout h-screen flex flex-col">
     <header class="relative border-b border-greyscale-200 px-5 py-4">
       <div class="flex items-center justify-between max-w-[545px] w-full m-auto">
-        <div class="flex items-center">
+        <router-link :to="{ name: 'Login' }" class="flex items-center">
           <img src="/images/logo.svg" alt="Logo" />
           <img src="/images/logo-text-dark.svg" alt="Logo text" class="ml-2" />
-        </div>
+        </router-link>
 
         <language-dropdown
           :has-overlay="false"
