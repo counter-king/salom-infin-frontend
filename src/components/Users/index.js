1c2b133797cff659d444e81c1d33c3ac70d62c1b
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-08-05 23:00:22 +0500

    feat: изменение место пользователей

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -3,3 +3,4 @@ export { default as UserWithCollapse } from "./WithCollapse.vue"
 export { default as UserWithLabel } from "./WithLabel.vue"
 export { default as UserWithRadio } from "./WithRadio.vue"
 export { default as UserWithSelectable } from "./WithSelectable.vue"
+export { default as UserWithSelectableItem } from './WithSelectableItem.vue'
