f16bf568e3eb82f3f1e5cea50a994d44b7d13fa3
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-11-16 21:58:16 +0500

    test content

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,15 @@
 import Toast from 'primevue/toast'
 // Components
 import Notification from '@/components/Notification.vue'
-import ReloadPrompt from '@/components/ReloadPrompt.vue'
-import Empty from "@/components/Empty.vue";
-import BaseButton from "@/components/UI/BaseButton.vue";
-import BaseAvatar from "@/components/UI/BaseAvatar.vue";
-import { VerticalCard, VerticalCardItem } from "@/components/Card/Vertical";
+// import ReloadPrompt from '@/components/ReloadPrompt.vue'
 </script>
 
 <template>
   <RouterView />
 
-  <base-input placeholder="asdasd" />
-
   <Toast />
 
   <notification />
 
-  <reload-prompt />
+<!--  <reload-prompt />-->
 </template>
