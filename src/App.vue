cc33d5066606b6b80b395c1101ebc2affe79245a
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-11-16 19:25:47 +0500

    test content

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,15 @@ import Toast from 'primevue/toast'
 // Components
 import Notification from '@/components/Notification.vue'
 import ReloadPrompt from '@/components/ReloadPrompt.vue'
+import BaseButton from "@/components/UI/BaseButton.vue";
 </script>
 
 <template>
   <RouterView />
 
   <Toast />
+
+  <base-button label="Test button" />
 
   <notification />
 
