a99bc057ce8e9c7108394815337d12d1d5e16945
Muhammad <imuhammadibragimov1994@gmail.com>
2024-11-21 16:20:08 +0500

    update vercel

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@ import Toast from 'primevue/toast'
 // Components
 import Notification from '@/components/Notification.vue'
 // import ReloadPrompt from '@/components/ReloadPrompt.vue'
-
 </script>
 
 <template>
