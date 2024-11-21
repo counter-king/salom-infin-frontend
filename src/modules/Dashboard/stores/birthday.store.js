360c6a2ab2127665f4b7a9af0b8975ec6ba78216
Azam_John <counterkingbarcelona@gmail.com>
2024-11-21 16:17:09 +0500

    change: dashboard birthday, slider, survey api connectd

---
+++ src/modules/Dashboard/stores/birthday.store.js
@@ -0,0 +1,113 @@
+// Core
+import { defineStore } from 'pinia'
+import {
+  fetchCongratulateUser,
+  fetchGetBirthdayList,
+  fetchGetBirthdayReactionCounts
+} from "@/modules/Dashboard/services/index.service";
+import { isBirthdayToday } from "@/utils";
+export const useDashboardBirthdayStore = defineStore('dashboardBirthdayStore', {
+  state: () => ({
+    tabItems: [
+      {
+        id: 1,
+        label: 'today',
+        active: true,
+        count: null
+      },
+      {
+        id: 2,
+        label: 'tomorrow',
+        active: false,
+        count: null
+      }
+    ],
+    wishItems: [
+      {
+        id: 1,
+        image: '/images/dashboard/gift-1.svg',
+        value: 'party_popper',
+        count: 12
+      },
+      {
+        id: 2,
+        image: '/images/dashboard/gift-2.svg',
+        value: 'cake',
+        count: 73
+      },
+      {
+        id: 3,
+        image: '/images/dashboard/gift-3.svg',
+        value: 'gift_box',
+        count: 45
+      },
+      {
+        id: 4,
+        image: '/images/dashboard/gift-4.svg',
+        value: 'champagne',
+        count: 24
+      }
+    ],
+    loading: true,
+    birthdayList: [],
+    todayBornList: [],
+    tomorrowBornList: [],
+    allBirthdays: []
+  }),
+  actions: {
+    async actionGetBirthdayList(params) {
+      try {
+        const res = await fetchGetBirthdayList(params)
+        if (res) {
+          for (const item of res.data) {
+            const response = await this.actionBirthdayReactionCounts(item.id)
+            this.allBirthdays.push({
+              ...item,
+              gifts: response.data.counts.map((gift, index) => ({
+                id: index + 1,
+                image: `/images/dashboard/gift-${index + 1}.svg`,
+                value: gift.reaction,
+                count: gift.count,
+                user_id: item.id,
+                disable: !!response.data.current_user_reaction,
+                selected: response.data.current_user_reaction === gift.reaction
+              }))
+            })
+          }
+
+          this.birthdayList = this.todayBornList = this.allBirthdays?.filter(item => isBirthdayToday(item.birth_date))
+          this.tomorrowBornList = this.allBirthdays?.filter(item => !isBirthdayToday(item.birth_date)).map(i => ({
+            ...i,
+            gifts: i.gifts.map(g => ({ ...g, disable: true }))
+          }))
+          this.tabItems[0].count = this.todayBornList.length || null
+          this.tabItems[1].count = this.tomorrowBornList.length || null
+          this.loading = false
+          return Promise.resolve(res.data)
+        }
+      } catch (err) {
+
+      }
+    },
+    async actionBirthdayReactionCounts(userId) {
+      try {
+        const res = await fetchGetBirthdayReactionCounts(userId)
+        if (res) {
+          return Promise.resolve(res)
+        }
+      } catch (err) {
+
+      }
+    },
+    async actionCongratulateUser(body) {
+      try {
+        const res = await fetchCongratulateUser(body)
+        if (res) {
+          return Promise.resolve(res)
+        }
+      } catch (err) {
+
+      }
+    }
+  }
+})
