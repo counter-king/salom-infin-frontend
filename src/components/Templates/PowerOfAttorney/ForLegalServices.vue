3ee424d177b6f0406c0a62d23b9023f23a0ef163
Azam_John <counterkingbarcelona@gmail.com>
2024-11-30 23:22:49 +0500

    build

--- src/components/Templates/PowerOfAttorney/ForLegalServices.vue
+++ src/components/Templates/PowerOfAttorney/ForLegalServices.vue
@@ -54,40 +54,40 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="for-legal-services-poa letter-template text-justify indent-8 text-sm">
-<!--    <pre>{{ props.empPassportDetails }}</pre>-->
+  <div class="for-legal-services-poa letter-template">
     <base-header-template />
-
-    <div class="text-base font-semibold text-center mt-8">ISHONCHNOMA</div>
-    <div class="mt-4">
-      Мazkur ishonchnoma orqali Toshkent shahri, Shahrisabz ko‘chasi, 3-uyda joylashgan “O‘zbekiston sanoat-qurilish banki” aksiyadorlik tijorat banki (keyingi o‘rinlarda – Bank) nomidan uning Ustavi asosida ish yurituvchi
+    <div class="text-justify indent-8 text-sm">
+      <div class="text-base font-semibold text-center mt-8">ISHONCHNOMA</div>
+      <div class="mt-4">
+        Мazkur ishonchnoma orqali Toshkent shahri, Shahrisabz ko‘chasi, 3-uyda joylashgan “O‘zbekiston sanoat-qurilish banki” aksiyadorlik tijorat banki (keyingi o‘rinlarda – Bank) nomidan uning Ustavi asosida ish yurituvchi
         <span class="font-semibold">
           {{ props.curatorPosition }} {{ props.curatorFullName }}
           {{ props.empPositionFull }} {{ props.empFullName }}
         </span>ga
-      O‘zbekiston Respublikasi Fuqarolik kodeksining 134-144-moddalariga asosan quyidagi vakolatlarni beradi
-    </div>
-    <div>
-      Bank va uning hududiy tarmoqlari faoliyatidan kelib chiqadigan sudlov ishlari boʼyicha Oʼzbekiston Respublikasining barcha sudlarida (iqtisodiy, fuqarolik, jinoyat, maʼmuriy, hakamlik) va barcha instansiyalarida, shuningdek Savdo-sanoat palatasi va uning hududiy boshqarmalarida, surishtiruv, tergov organlari, Majburiy ijro byurosi va uning hududiy boʼlimlari va boshqa davlat organlari va tashkilotlarida bankning ishonchli vakili sifatida ishtirok etish;
-    </div>
-    <div>
-      Sudlarda bank nomidan barcha protsessual harakatlarni amalga oshirish, xususan da’vo ariza, ariza, iltimos va shikoyatni imzolash, sud hujjati ustidan apellyatsiya, kassatsiya, taftish shikoyatlarini hamda qarzdorga nisbatan to‘lovga qobiliyatsizlik to‘g‘risida ish qo‘zg‘atish haqidagi arizani imzolash, shuningdek sud hujjatini yangi ochilgan holatlar bo‘yicha qayta ko‘rib chiqish to‘g‘risidagi arizani imzolash, ishni hakamlik sudiga topshirish, da’vo (arz qilingan talab) predmetini yoki asosini o‘zgartirish, da’vo talablari miqdorini ko‘paytirish yoki kamaytirish, ijro hujjatini undiruvga taqdim etish, ijro hujjatini ijroga topshirish, sud hujjatining majburiy ijro etilishini talab qilish, undirilgan mol-mulkni yoki pulni olish, davlat ijrochisining xatti-harakatlari (harakat yoki harakatsizligi) ustidan shikoyat berish va uni rad qilish hamda ushbu ishonchnoma bilan yuklatilgan topshiriqlarni bajarish bilan bog’liq bo‘lgan boshqa zarur harakatlarni bajarish;
-    </div>
-    <div>
-      Bank faoliyatidan kelib chiqadigan masalalar yuzasidan huquqni muhofaza qiluvchi organlarga tegishli tartibda ariza, shikoyat, takliflar hamda amaliy yordam ko‘rsatish to‘g’risidagi murojaatlarni imzolash;
-    </div>
-    <div>
-      To‘lovga qobiliyatsizlik to‘g‘risidagi ishlar bo’yicha Bankning ishonchli vakili sifatida ishtirok etish, ko’rsatma va tushuntirishlar berish, kreditorlar yig’ilishida vakil sifatida ishtirok etish, shuningdek notarial idora (notarius)larda garov va ipoteka shartnomalarini hamda ularga kiritiladigan o’zgartirish va qo’shimchalarni Bank nomidan imzolash, shu bilan birga ijro xatlarini rasmiylashtirish uchun notariuslarga barcha zarur hujjatlarni taqdim etish va ijro xatlarini ulardan olish vakolatini beradi.
-    </div>
-    <div>
-      {{ props.empPositionFull }} {{ props.empFullName }}ning ID pasport ma‘lumotlari:
-      {{ props.empPassportDetails }}.
-    </div>
-    <div class="italic">
-      Ushbu ishonchnomaning berilishi munosabati bilan 11.07.2024-yilda berilgan 04-07/153-sonli ishonchnoma o‘z kuchini yo‘qotadi.
-      Mazkur ishonchnomaning amal qilish muddati u orqali berilgan vakolatlarni boshqa shaxsga o‘tkazish huquqisiz
-      <span class="font-semibold">{{ formatDate(props.composeModel.start_date) }} dan {{ formatDate(props.composeModel.end_date) }} gacha</span> berildi.
-      Ushbu ishonchnoma orqali ishonch bildiriluvchi shaxs boshqa lavozimga o’tkazilganda yoki mehnat shartnomasi bekor qilinganda o’z kuchini yo‘qotadi.
+        O‘zbekiston Respublikasi Fuqarolik kodeksining 134-144-moddalariga asosan quyidagi vakolatlarni beradi
+      </div>
+      <div>
+        Bank va uning hududiy tarmoqlari faoliyatidan kelib chiqadigan sudlov ishlari boʼyicha Oʼzbekiston Respublikasining barcha sudlarida (iqtisodiy, fuqarolik, jinoyat, maʼmuriy, hakamlik) va barcha instansiyalarida, shuningdek Savdo-sanoat palatasi va uning hududiy boshqarmalarida, surishtiruv, tergov organlari, Majburiy ijro byurosi va uning hududiy boʼlimlari va boshqa davlat organlari va tashkilotlarida bankning ishonchli vakili sifatida ishtirok etish;
+      </div>
+      <div>
+        Sudlarda bank nomidan barcha protsessual harakatlarni amalga oshirish, xususan da’vo ariza, ariza, iltimos va shikoyatni imzolash, sud hujjati ustidan apellyatsiya, kassatsiya, taftish shikoyatlarini hamda qarzdorga nisbatan to‘lovga qobiliyatsizlik to‘g‘risida ish qo‘zg‘atish haqidagi arizani imzolash, shuningdek sud hujjatini yangi ochilgan holatlar bo‘yicha qayta ko‘rib chiqish to‘g‘risidagi arizani imzolash, ishni hakamlik sudiga topshirish, da’vo (arz qilingan talab) predmetini yoki asosini o‘zgartirish, da’vo talablari miqdorini ko‘paytirish yoki kamaytirish, ijro hujjatini undiruvga taqdim etish, ijro hujjatini ijroga topshirish, sud hujjatining majburiy ijro etilishini talab qilish, undirilgan mol-mulkni yoki pulni olish, davlat ijrochisining xatti-harakatlari (harakat yoki harakatsizligi) ustidan shikoyat berish va uni rad qilish hamda ushbu ishonchnoma bilan yuklatilgan topshiriqlarni bajarish bilan bog’liq bo‘lgan boshqa zarur harakatlarni bajarish;
+      </div>
+      <div>
+        Bank faoliyatidan kelib chiqadigan masalalar yuzasidan huquqni muhofaza qiluvchi organlarga tegishli tartibda ariza, shikoyat, takliflar hamda amaliy yordam ko‘rsatish to‘g’risidagi murojaatlarni imzolash;
+      </div>
+      <div>
+        To‘lovga qobiliyatsizlik to‘g‘risidagi ishlar bo’yicha Bankning ishonchli vakili sifatida ishtirok etish, ko’rsatma va tushuntirishlar berish, kreditorlar yig’ilishida vakil sifatida ishtirok etish, shuningdek notarial idora (notarius)larda garov va ipoteka shartnomalarini hamda ularga kiritiladigan o’zgartirish va qo’shimchalarni Bank nomidan imzolash, shu bilan birga ijro xatlarini rasmiylashtirish uchun notariuslarga barcha zarur hujjatlarni taqdim etish va ijro xatlarini ulardan olish vakolatini beradi.
+      </div>
+      <div>
+        {{ props.empPositionFull }} {{ props.empFullName }}ning ID pasport ma‘lumotlari:
+        {{ props.empPassportDetails }}.
+      </div>
+      <div class="italic">
+        Ushbu ishonchnomaning berilishi munosabati bilan 11.07.2024-yilda berilgan 04-07/153-sonli ishonchnoma o‘z kuchini yo‘qotadi.
+        Mazkur ishonchnomaning amal qilish muddati u orqali berilgan vakolatlarni boshqa shaxsga o‘tkazish huquqisiz
+        <span class="font-semibold">{{ formatDate(props.composeModel.start_date) }} dan {{ formatDate(props.composeModel.end_date) }} gacha</span> berildi.
+        Ushbu ishonchnoma orqali ishonch bildiriluvchi shaxs boshqa lavozimga o’tkazilganda yoki mehnat shartnomasi bekor qilinganda o’z kuchini yo‘qotadi.
+      </div>
     </div>
 
     <div class="mt-6 pb-2 px-4">
