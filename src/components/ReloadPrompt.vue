91456511cfe6bf1df4731dbe4a90c83b8a4353a0
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-12-12 23:06:35 +0500

    refactor: редизайн reload баннер

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -1,17 +1,23 @@
 <script setup>
 // Core
-import { computed, ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useRegisterSW } from 'virtual:pwa-register/vue'
 import { useRoute } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 // Components
-import { RefreshCircleBoldIcon } from '@/components/Icons'
-// Stores
-import { useNavigationStore } from '@/stores/navigation.store'
+import { RefreshCircleBoldIcon, ChevronDown20SolidIcon, ChevronUp20SolidIcon } from '@/components/Icons'
 // Composable
 const route = useRoute()
 const { t } = useI18n()
-const navigationStore = useNavigationStore()
+// Reactive
+const isMinimized = ref(false)
+// Watch
+watch(() => route.fullPath, () => isMinimized.value = false)
+// Methods
+const minimize = () => {
+  isMinimized.value = !isMinimized.value
+}
+
 // periodic sync is disabled, change the value to enable it, the period is in milliseconds
 // You can remove onRegisteredSW callback and registerPeriodicSync function
 const period = 0
@@ -42,7 +48,7 @@ function registerPeriodicSync(swUrl, r) {
   }, period)
 }
 
-const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW({
+const { needRefresh, updateServiceWorker } = useRegisterSW({
   immediate: true,
   onRegisteredSW(swUrl, r) {
 
@@ -61,83 +67,73 @@ const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW({
     }
   },
 })
-
-const title = computed(() => {
-  if (offlineReady.value)
-    return t('application-ready-work-offline')
-  if (needRefresh.value)
-    return t('text-new-version-available')
-  return ''
-})
-
-const bannerFullWidth = computed(() => {
-  return route.name === 'Calendar'
-    || route.name === 'DashboardIndex'
-    || route.name === 'HandbookIndex'
-    || route.name === 'Login'
-    || route.name === 'ConfirmNumber'
-    || route.name === 'VerifyNumber'
-    || route.name === 'SetCredentials'
-    || route.name === 'ForgetPassword'
-})
-
-function close() {
-  offlineReady.value = false
-  needRefresh.value = false
-}
 </script>
 
 <template>
   <div
-    v-if="offlineReady || needRefresh"
-    class="flex gap-3 items-center fixed bottom-0 right-0 w-full transition-all max-w-[calc(100vw-250px)] bg-success-500 text-white z-[9999] py-[7px] px-6"
+    v-if="needRefresh"
+    class="flex items-center justify-center fixed bottom-0 right-0 w-full transition-all duration-500 delay-500 max-w-[100vw] h-[156px] bg-greyscale-50 border-t border-t-greyscale-200 z-[99] p-3"
     :class="{
-      'max-w-[calc(100vw-76px)] duration-500 delay-500': navigationStore.sidebarCollapse,
-      '!max-w-[100vw] duration-500 delay-500': bannerFullWidth
+      '!h-16': isMinimized
     }"
   >
-    <base-iconify :icon="RefreshCircleBoldIcon" class="!w-7 !h-7" />
-
-    <span class="font-semibold mr-2">{{ t(title) }}</span>
-
-    <base-button
-      v-if="offlineReady"
-      severity="success"
-      rounded
-      size="small"
-      button-class="!bg-[#8ACB6E] shadow-[0px_1px_3px_0px_#00000026] !border-none"
-      @click="close"
+    <div
+      class="flex items-center gap-5 max-w-[1000px] w-full transition-all duration-500 delay-500"
+      :class="{ '!gap-4': isMinimized }"
     >
-      <template #label>
-        <span class="!text-sm">{{ t('close') }}</span>
-      </template>
-    </base-button>
+      <div
+        class="flex items-center justify-center w-14 h-14 bg-success-500 rounded-full transition-all duration-500 delay-500"
+        :class="{ '!w-10 !h-10': isMinimized }"
+      >
+        <base-iconify
+          :icon="RefreshCircleBoldIcon"
+          class="transition-all duration-500 delay-500 text-white"
+          :class="{ '!w-8 !h-8': !isMinimized }"
+        />
+      </div>
 
-    <base-button
-      v-if="needRefresh"
-      severity="success"
-      rounded
-      size="small"
-      button-class="!bg-[#8ACB6E] shadow-[0px_1px_3px_0px_#00000026] !border-none"
-      @click="updateServiceWorker()"
+      <div class="flex-1">
+        <h1
+          class="text-xl font-semibold text-greyscale-900 transition-all duration-500 delay-500"
+          :class="{ '!text-base -mb-1': isMinimized }"
+        >
+          {{ t('text-new-version-available') }}
+        </h1>
+
+        <span
+          class="text-sm text-greyscale-500 transition-all duration-500 delay-500"
+          :class="{ '!text-[13px]': isMinimized }"
+        >
+          {{ t('text-new-version-available-sub') }}
+        </span>
+      </div>
+
+      <base-button
+        v-if="needRefresh"
+        rounded
+        :size="isMinimized ? 'small' : 'large'"
+        @click="updateServiceWorker()"
+      >
+        <template #label>
+          <span class="!text-sm">{{ t('renew') }}</span>
+        </template>
+      </base-button>
+    </div>
+
+    <div
+      class="group flex items-center justify-center w-6 h-6 bg-white hover:bg-primary-500 rounded-[6px] absolute top-5 right-5 cursor-pointer"
+      v-tooltip.left="{
+        value: `<h4 class='text-xs text-white -my-1'>${ isMinimized ? t('expand') : t('collapse') }</h4>`,
+        escape: true,
+        autoHide: false
+      }"
+      @click="minimize"
     >
-      <template #label>
-        <span class="!text-sm">{{ t('renew') }}</span>
-      </template>
-    </base-button>
-<!--    <div class="message">-->
-<!--      <span id="toast-message">-->
-<!--        {{ title }}-->
-<!--      </span>-->
-<!--    </div>-->
-<!--    <div class="buttons">-->
-<!--      <button v-if="needRefresh" type="button" class="reload" @click="updateServiceWorker()">-->
-<!--        Обновить-->
-<!--      </button>-->
-<!--      <button type="button" @click="close">-->
-<!--        Закрыть-->
-<!--      </button>-->
-<!--    </div>-->
+      <base-iconify
+        :icon="isMinimized ? ChevronUp20SolidIcon : ChevronDown20SolidIcon"
+        class="group-hover:text-white text-primary-500"
+      />
+    </div>
   </div>
 </template>
 
