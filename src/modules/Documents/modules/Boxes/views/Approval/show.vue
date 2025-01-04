f8d0c0086bd79b8f3ca416098f2bb0df07b98c62
Azam_John <counterkingbarcelona@gmail.com>
2025-01-04 17:52:39 +0500

    change: new business trip notice changed

--- src/modules/Documents/modules/Boxes/views/Approval/show.vue
+++ src/modules/Documents/modules/Boxes/views/Approval/show.vue
@@ -155,7 +155,7 @@ onMounted(  () => {
           class="p-4 overflow-y-auto"
           style="height: calc(100vh - 250px)"
         >
-          <div class="p-10 pl-[80px] min-h-full shadow-block border-[0.095rem] border-greyscale-200">
+          <div class="p-10 min-h-full shadow-block border-[0.095rem] border-greyscale-200">
             <base-template
               v-if="approvalStore.detailModel && approvalStore.detailModel.compose"
               :compose-model="approvalStore.detailModel?.compose"
