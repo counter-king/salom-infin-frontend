75f8f73dea9e4a2615af806d8a6d5fbf9f267c4a
Azam_John <counterkingbarcelona@gmail.com>
2025-01-06 19:58:41 +0500

    build

--- src/modules/Documents/modules/SendDocuments/stores/index.store.js
+++ src/modules/Documents/modules/SendDocuments/stores/index.store.js
@@ -246,14 +246,14 @@ export const useSDStore = defineStore("sd-stores", {
             documentType: COMPOSE_DOCUMENT_TYPES.NOTICE,
             documentSubType: COMPOSE_DOCUMENT_SUB_TYPES.ORDINARY_NOTICE
           },
-          {
-            label: 'business-trip',
-            type: 'submenu',
-            hasRouterLink: true,
-            routerLinkName: ROUTE_SD_CREATE,
-            documentType: COMPOSE_DOCUMENT_TYPES.NOTICE,
-            documentSubType: COMPOSE_DOCUMENT_SUB_TYPES.BUSINESS_TRIP
-          },
+          // {
+          //   label: 'business-trip',
+          //   type: 'submenu',
+          //   hasRouterLink: true,
+          //   routerLinkName: ROUTE_SD_CREATE,
+          //   documentType: COMPOSE_DOCUMENT_TYPES.NOTICE,
+          //   documentSubType: COMPOSE_DOCUMENT_SUB_TYPES.BUSINESS_TRIP
+          // },
           {
             label: 'business-trip-v2',
             type: 'submenu',
