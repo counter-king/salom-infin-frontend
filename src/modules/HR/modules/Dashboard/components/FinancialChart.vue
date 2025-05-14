86f6022a530f0ef29fd9d6add4cf9ec5f8f3ba18
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2025-05-15 00:16:00 +0500

    feat: set current month in chart salary

--- src/modules/HR/modules/Dashboard/components/FinancialChart.vue
+++ src/modules/HR/modules/Dashboard/components/FinancialChart.vue
@@ -1,9 +1,12 @@
 <script setup>
 import FinancialChartColumn from './FinancialChartColumn.vue'
+import FinancialChartBar from './FinancialChartBar.vue'
 </script>
 
 <template>
-  <div>
+  <div class="space-y-5">
     <financial-chart-column />
+
+<!--    <financial-chart-bar />-->
   </div>
 </template>
